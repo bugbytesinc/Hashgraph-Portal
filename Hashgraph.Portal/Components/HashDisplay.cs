706a6fc631675368e722a1863c9d5e3ab0a64598
Jason Fabritz <msdn@bugbytes.com>
2020-01-28 21:29:23 -0500

    Fixes #12 Get Account Records

---
+++ Hashgraph.Portal/Components/HashDisplay.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace Hashgraph.Portal.Components
+{
+    public class HashDisplay : ComponentBase
+    {
+        [Parameter] public ReadOnlyMemory<byte> Hash { get; set; }
+        [Parameter(CaptureUnmatchedValues = true)] public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            if (builder != null)
+            {
+                builder.OpenElement(0, "span");
+                builder.AddMultipleAttributes(1, AdditionalAttributes);
+                if (Hash.IsEmpty)
+                {
+                    builder.AddContent(2, "Empty");
+                }
+                else
+                {
+                    builder.AddContent(2, Hex.FromBytes(Hash));
+
+                }
+                builder.CloseElement();
+            }
+        }
+    }
+}
