696692048be74a8899523eb87ab760cf3aeec528
Jason Fabritz <msdn@bugbytes.com>
2020-06-10 07:26:11 -0400

    Delete Contract

---
+++ Hashgraph.Portal/Pages/DeleteContract.cs
@@ -0,0 +1,46 @@
+﻿using Hashgraph.Portal.Components;
+using Hashgraph.Portal.Services;
+using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Hashgraph.Portal.Pages
+{
+    public partial class DeleteContract : ComponentBase
+    {
+        [Inject] public DefaultsService DefaultsService { get; set; }
+
+        private Network _network = null;
+        private DeleteContractInput _input = new DeleteContractInput();
+        private TransactionReceipt _output = null;
+
+        protected override void OnInitialized()
+        {
+            _input.Gateway = DefaultsService.Gateway;
+            _input.Payer = DefaultsService.Payer;
+            base.OnInitialized();
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            _output = null;
+            await _network.ExecuteAsync(_input.Gateway, _input.Payer, async client =>
+            {
+                _output = await client.DeleteContractAsync(_input.Contract, _input.TransferToAddress, ctx => ctx.Memo = _input.Memo?.Trim());
+            });
+        }
+    }
+    public class DeleteContractInput
+    {
+        [Required(ErrorMessage = "Please select a Network Gateway Node.")]
+        public Gateway Gateway { get; set; }
+        [Required(ErrorMessage = "Please enter the Contract that will pay the Transaction Fees.")]
+        public Address Payer { get; set; }
+        [Required(ErrorMessage = "Please enter the Contract you wish to delete.")]
+        public Address Contract { get; set; }
+        [Required(ErrorMessage = "Please enter the address that will receive the remaining funds from the deleted contract.")]
+        public Address TransferToAddress { get; set; }
+        [MaxLength(100, ErrorMessage = "The memo field cannot exceed 100 characters.")]
+        public string Memo { get; set; }
+    }
+}
