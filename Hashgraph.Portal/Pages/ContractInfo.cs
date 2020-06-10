97f8deb240ad2dc425704d0114011f9b72ed1564
Jason Fabritz <msdn@bugbytes.com>
2020-06-10 07:03:56 -0400

    Contract Info

---
+++ Hashgraph.Portal/Pages/ContractInfo.cs
@@ -0,0 +1,43 @@
+﻿using Hashgraph.Portal.Components;
+using Hashgraph.Portal.Services;
+using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Hashgraph.Portal.Pages
+{
+    public partial class ContractInfo : ComponentBase
+    {
+        [Inject] public DefaultsService DefaultsService { get; set; }
+
+        private Network _network = null;
+        private ContractInfoInput _input = new ContractInfoInput();
+        private Hashgraph.ContractInfo _output = null;
+
+        protected override void OnInitialized()
+        {
+            _input.Payer = DefaultsService.Payer;
+            _input.Gateway = DefaultsService.Gateway;
+            base.OnInitialized();
+        }
+        protected async Task HandleValidSubmit()
+        {
+            _output = null;
+            await _network.ExecuteAsync(_input.Gateway, _input.Payer, async client =>
+            {
+                _output = await client.GetContractInfoAsync(_input.Contract, ctx => ctx.Memo = _input.Memo?.Trim());
+            });
+        }
+    }
+    public class ContractInfoInput
+    {
+        [Required(ErrorMessage = "Please select a Network Gateway Node.")]
+        public Gateway Gateway { get; set; }
+        [Required(ErrorMessage = "Please enter the account that will pay the Query Transaction Fees.")]
+        public Address Payer { get; set; }
+        [Required(ErrorMessage = "Please enter the Contract you wish to query.")]
+        public Address Contract { get; set; }
+        [MaxLength(100, ErrorMessage = "The memo field cannot exceed 100 characters.")]
+        public string Memo { get; set; }
+    }
+}
